98503f722ef1c7b03f7ce3afbdf54bb298864dd4
David Peterson <david@randombits.org>
2022-01-06 00:24:35 +1000

    * Removed Quick/Nimble dependencies.

--- Package.swift
+++ Package.swift
@@ -14,8 +14,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/pointfreeco/swift-parsing.git", branch: "pb-pp"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,6 +23,6 @@ let package = Package(
             dependencies: [.product(name: "Parsing", package: "swift-parsing")]),
         .testTarget(
             name: "ParsingAsyncTests",
-            dependencies: ["ParsingAsync", "Nimble", "Quick"]),
+            dependencies: ["ParsingAsync"]),
     ]
 )
